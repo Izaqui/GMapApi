200e715e09a48fd7ea353cf93e9f79c057154e87
izkiel12345 <izaquiel.silva.ads@gmail.com>
2021-12-21 14:27:25 -0300

    Api com google maps

---
+++ map.js
@@ -0,0 +1,128 @@
+let map;
+let marker;
+
+let center = {lat: -6.888463202449027, lng: -38.558930105104125};
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: center,
+    zoom: 12,
+  });
+  
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Latitude: " + position.coords.latitude + 
+          "<br>Longitude: " + position.coords.longitude);
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+    alert(pos)
+  });
+  const informe = new google.maps.InfoWindow();
+  informe.setContent("Marcado");
+  map.addListener('click', evt =>{
+    let marker = new google.maps.Marker({
+      position: evt.latLng,
+      map: map
+    });
+    informe.open(map, marker);
+
+  });
+  
+  const geocoder = new google.maps.Geocoder();
+  const infowindow = new google.maps.InfoWindow();
+  document.getElementById("submit").addEventListener("click", () => {
+    geocodeLatLng(geocoder, map, infowindow);
+  });
+  marker = new google.maps.Marker({
+    map: map,
+    position: center,
+    draggable: true
+});
+
+map.addListener("click", (evt) => {
+  addMarker(evt);
+});
+
+marker.addListener('position_changed', ()=>{
+    map.setCenter(marker.position);
+});
+  
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+function geocodeLatLng(geocoder, map, infowindow) {
+  const input = document.getElementById("latlng").value;
+  const latlngStr = input.split(",", 2);
+  const latlng = {
+    lat: parseFloat(latlngStr[0]),
+    lng: parseFloat(latlngStr[1]),
+  };
+  geocoder
+    .geocode({ location: latlng })
+    .then((response) => {
+      if (response.results[0]) {
+        map.setZoom(11);
+        const marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+        });
+        infowindow.setContent(response.results[0].formatted_address);
+        infowindow.open(map, marker);
+      } else {
+        window.alert("No results found");
+      }
+    })
+    .catch((e) => window.alert("Geocoder failed due to: " + e));
+}
+function addMarker(evt){
+    marker.setPosition(evt.latLng);
+}
+
+function salvar(){
+
+    const obj = {
+        nome: document.getElementById('nome').value,
+        lat: marker.getPosition().lat(),
+        lng: marker.getPosition().lng()
+    };
+
+    fetch("http://localhost:3000/pontos",{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+    }).then(response =>{alert('Inserido!')})
+    .catch(error => alert('Falha ao salvar!'));    
+
+}
